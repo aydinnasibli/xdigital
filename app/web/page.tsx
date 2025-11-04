98dbae0f6efc0bab32aeff4ead9e89657b2ac0b9
Aydin Nasibli <58357722+aydinnasibli@users.noreply.github.com>
2025-11-04 16:26:18 +0300

    Update page.tsx

--- app/web/page.tsx
+++ app/web/page.tsx
@@ -1,11 +1,11 @@
 import { getFaqWeb, getPricingPackages } from '@/lib/sanityQueries'
 import WebPageClient from './WebPageClient'
 
-// This tells Next.js to revalidate this page every hour
+// This tells Next.js to revalidate this page every day
 export const revalidate = 86400
 
 export default async function WebPage() {
-    // Fetch data on the server at build time and every hour
+    // Fetch data on the server at build time and every day
     const [faqs, packages] = await Promise.all([
         getFaqWeb(),
         getPricingPackages()
